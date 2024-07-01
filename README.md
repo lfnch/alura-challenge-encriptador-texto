690054e9dee9a55e8dee4b25b7a57ede70ed6575
Felipe Nieves <lfnch@outlook.com>
2024-06-30 23:04:23 -0500

    Se actualiza la logica javascript

--- README.md
+++ README.md
@@ -20,8 +20,11 @@ La letra "u" es convertida para "ufat"
 Requisitos:
 
 Debe funcionar solo con letras minúsculas
+
 No deben ser utilizados letras con acentos ni caracteres especiales
+
 Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+
 Por ejemplo:
 "gato" => "gaitober"
 gaitober" => "gato"
--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 /*
-*
+* Luis felipe nieves ch
 */
 
 //Optenemos la instancia de los botones y elementos de html con los que vamos a trabajar
@@ -9,6 +9,7 @@ const INPUT_RESULTADO  = document.querySelector("#input_resultado");
 const BTN_ENCRIPTAR    = document.querySelector("#btn_encriptar");
 const BTN_DESENCRIPTAR = document.querySelector("#btn_desencriptar");
 const BTN_COPIAR       = document.querySelector("#btn_copiar");
+let validacion_correcta= true;
 
 /*
 * restablecerFormulario
@@ -19,8 +20,8 @@ function restablecerFormulario()
 {
     //Limpiamos los campos de texto
     mostrarMensaje('');
-    INPUT_TEXTO.value     = "";
-    INPUT_RESULTADO.value = "";
+    INPUT_TEXTO.value     = '';
+    INPUT_RESULTADO.value = '';
 
     //Restablecemos los botonos
     cambiarEstadoBotones(true);
@@ -38,15 +39,14 @@ function mostrarMensaje(msj)
     SPAN_MENSAJE.innerHTML = msj;
 }
 
-
 /*
-* inputEstaVacio
+* textoVacio
 * Valida que la entrada de texto no exte vacia
 * @return : boolean
 */
-function inputEstaVacio()
+function textoVacio(texto)
 {   
-   return (INPUT_TEXTO.value.trim() != '') ? false: true;
+   return (texto != '') ? false: true;
 }
 
 /*
@@ -72,64 +72,260 @@ function cambiarEstadoBotonCopiar(estado)
     BTN_COPIAR.disabled = estado;
 }
 
-function esLetra(caracter)
-{
-    return /^[a-zA-Z]$/.test(caracter);
-}
-
+/*
+* esCaracterEspecial
+* @param : caracter => char
+* @return boolean
+*/
 function esCaracterEspecial(caracter)
 {
-    return /[^a-zA-Z0-9]/.test(caracter);
+    return /[^a-zA-Z0-9 ]/.test(caracter);
 }
 
-function validar()
+/*
+* validarCaracteresPermitidos
+* Valida cada caracter para definir si es especial o no, ya que caracteres especiales no son permitidos
+* @void
+*/
+function validarCaracteresPermitidos()
 {
     cambiarEstadoBotones(true);
+    cambiarEstadoBotonCopiar(true);
     mostrarMensaje('');
-    if(!inputEstaVacio())
+    INPUT_RESULTADO.value = '';
+
+    //Obtenemos el texto a encriptar
+    let texto = INPUT_TEXTO.value.trim().toLowerCase();
+    
+    //Validamos campo no este vacio
+    if(!textoVacio(texto))
     {
-        //Obtenemos el ultimo caracter escrito
+       //Obtenemos el ultimo caracter escrito
         const ULTIMO_CARACTER = INPUT_TEXTO.value.charAt(INPUT_TEXTO.value.length -1);
         
         //Comprobamos que no sea caracter especial
         if(!esCaracterEspecial(ULTIMO_CARACTER))
         {
-            INPUT_RESULTADO.value = ULTIMO_CARACTER;
+            validacion_correcta = true;
+            //Activamos los botones
+            cambiarEstadoBotones(false)
         }
         else
         {
+            validacion_correcta = false;
             mostrarMensaje('No se permiten caracteres especiales.');
+            INPUT_TEXTO.value = texto.slice(0, -1);
         }
+    }
+    else
+    {
+        validacion_correcta = false;
+        INPUT_TEXTO.value = '';
     }
 }
 
+/*
+* comprobarTextoYaEncriptado
+* valida si el texto posiblemente esta encritado
+* @param : texto => cadena
+* @return boolean
+*/
+function comprobarTextoYaEncriptado(texto)
+{
+    //Establecemos las palabras clave
+    let palabras_clave = ['ai', 'enter', 'imes', 'ober', 'ufat'];
+    let contador = 0;
 
-restablecerFormulario();
+    //recorremos las palabras clave
+    palabras_clave.forEach(palabra_clave => 
+    {
+        //comprobamos que este incluida en una palabra del texto
+        if(texto.includes(palabra_clave))
+        {
+            contador++;
+        }
+    });
+    
+    return (contador > 0) ? true : false;
+}
+
+/*
+* encriptarTexto
+* 
+* @param : texto => cadena
+* @return cadena
+*/
+function encriptarTexto(texto)
+{
+    let texto_encriptado = '';
+
+    for(i = 0; i < texto.length; i++)
+    {
+        switch(texto[i]) 
+        {
+            case 'a' :
+                texto_encriptado += 'ai';
+            break;
+            case 'e' :
+                texto_encriptado += 'enter';
+            break;
+            case 'i' :
+                texto_encriptado += 'imes';
+            break;
+            case 'o' :
+                texto_encriptado += 'ober';
+            break;
+            case 'u' :
+                texto_encriptado += 'ufat';
+            break;
+            default :
+                texto_encriptado += texto[i];
+            break;
+        }
+    }
+    
+    return texto_encriptado;
+}
+
+function desencriptarTexto(texto)
+{
+    //Establecemos las palabras clave
+    let palabras_clave = [['a', 'ai'], ['e', 'enter'], ['i', 'imes'], ['o', 'ober'], ['u', 'ufat']];
+
+    palabras_clave.forEach(palabra_clave => 
+    {
+        texto = texto.replace(new RegExp(palabra_clave[1], 'g'), palabra_clave[0]);
+    });
+
+    return texto;
+
+    felicidades
+}
+
+function mostrarResultado(texto)
+{
+    INPUT_RESULTADO.value = texto;
+    if(!textoVacio(texto))
+    {
+        cambiarEstadoBotonCopiar(false);
+    }
+}
+
+/*
+* encriptar
+* 
+* @param : 
+* @return
+*/
+function encriptar()
+{
+    if(validacion_correcta)
+    {
+        //Obtenemos el texto a encriptar
+        let texto = INPUT_TEXTO.value.trim().toLowerCase();
+        if(!comprobarTextoYaEncriptado(texto))
+        {
+            mostrarResultado(encriptarTexto(texto));
+        }
+        else
+        {
+            let confirmado = confirm(
+                `Parece que el texto esta ya encriptado.\n¿Desea continuar?`);
+            if(confirmado)
+            {
+                mostrarResultado(encriptarTexto(texto));
+            }
+        }
+    }
+    else
+    {
+        mostrarMensaje('No se superaron las validaciones.');
+    }
+}
+
+/*
+* desencriptar
+* 
+* @param : 
+* @return
+*/
+function desencriptar()
+{
+    if(validacion_correcta)
+    {
+        //Obtenemos el texto a encriptar
+        let texto = INPUT_TEXTO.value.trim().toLowerCase();
+        if(comprobarTextoYaEncriptado(texto))
+        {
+            mostrarResultado(desencriptarTexto(texto));
+        }
+        else
+        {
+            let confirmado = confirm(
+                `Parece que el texto no esta encriptado.\n¿Desea continuar?`);
+            if(confirmado)
+            {
+                mostrarResultado(desencriptarTexto(texto));
+            }
+        }
+    }
+    else
+    {
+        mostrarMensaje('No se superaron las validaciones.');
+    }
+}
+
+/*
+* copiar
+* 
+* @param : 
+* @return
+*/
+function copiar()
+{
+    let texto = INPUT_RESULTADO.value.trim().toLowerCase();
+    if(!textoVacio(texto))
+    {
+        navigator.clipboard.writeText(texto)
+        .then(() => 
+        {
+            console.log('Contenido copiado al portapapeles');
+            /* Resuelto - texto copiado al portapapeles con éxito */
+        },
+        () => 
+        {
+            console.error('Error al copiar');
+            /* Rechazado - fallo al copiar el texto al portapapeles */
+        });
+    }
+}
 
 /*
 * Eventos
 */
 
+restablecerFormulario();
+
 //Evento al escribir en el campo
 INPUT_TEXTO.addEventListener('keyup', () => 
 {
-    validar();
+    validarCaracteresPermitidos();
 });
 
 //Evento click en boton encriptar
 BTN_ENCRIPTAR.addEventListener('click', () => 
 {
-   
+    encriptar();
 });
 
 //Evento click en boton desencriptar
 BTN_DESENCRIPTAR.addEventListener('click', () => 
 {
-       
+    desencriptar();
 });
 
 //Evento click en boton copiar
 BTN_COPIAR.addEventListener('click', () => 
 {
-   
+    copiar();
 });
